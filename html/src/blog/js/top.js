3056809a997151af1e50de7a391865906ca1c419
deepke <deepke@deepke.cn>
2018-01-04 22:11:02 -0500

    文件删除

--- html/src/blog/js/top.js
+++
@@ -1,9 +0,0 @@
-(function () {
-	$.post(path+"/user/login_info",null,function(res){
-		document.getElementById("login").innerHTML=res;
-	});
-	if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-		window.location.href="/mwebgxblog";
-	}
-})();
-		
